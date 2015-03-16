798f78018018d93e2ad56b0707e3065e624fe204
Shalhout Shalhout <sshalhou@cern.ch>
2015-03-16 13:47:24 +0100

    testing config pset

---
+++ UserCode/TupleConfigurations/ConfigTupleElectrons_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+###################################
+# configuration file to determine
+# what mini-AOD skimmedElectrons are retained
+# in TupleElectron collection
+# will define :
+# thinCuts = minimum skim requirements on electrons (anything failing these will be thrown away)
+# vetoCuts = minimum requirement to be a veto electron
+# signal_eleTauCuts = minimum requirement to be a signal ele + Tau_h final state electron
+# signal_eleMuCuts = minimum requirement to be a signal ele + muon final state electron
+###################################
+
+thinCuts = cms.PSet(minPt = cms.double(10.0))
+vetoCuts = cms.PSet(minPt = cms.double(10.0))
+signal_eleTauCuts = cms.PSet(minPt = cms.double(10.0))
+signal_eleMuCuts = cms.PSet(minPt = cms.double(10.0))
