b87ab9df9cfc43975328f00c3287edd8dbb33d3e
gfunk723 <gfunk723@gmail.com>
2016-05-23 15:03:42 -0500

    configure for condor and plain submission

--- AnalysisRunII/Analysis/operation_submit.py
+++ AnalysisRunII/Analysis/operation_submit.py
@@ -2,6 +2,10 @@
 import subprocess
 import os
 import sys, getopt
+import textwrap
+import time
+
+execTitle = ""
 
 def getOptions(argv):
   useCondor = 0
@@ -12,11 +16,11 @@ def getOptions(argv):
   try:
     opts, args = getopt.getopt(argv,"hct:i:o:d:",["tTree=","iFile=","opList=","outDir="])
   except getopt.GetoptError:
-    print 'Usage: python operation_submit.py -c (to use condor) -i [TTree in FlatTuple] -i [input FlatTuple root file or txt file with list of root files] -o [operations to perform] -d [output path]'
+    print 'Usage: python operation_submit.py -c (to use condor) -i [TTree in FlatTuple] -i [input name and path to FlatTuple root file or txt file with list of root files] -o [operations to perform] -d [output path]'
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print 'Usage: python operation_submit.py -c (to use condor) -t [TTree in FlatTuple] -i [input FlatTuple root file or txt file with list of root files] -o [operations to perform] -d [output path]'
+      print 'Usage: python operation_submit.py -c (to use condor) -t [TTree in FlatTuple] -i [input name and path to FlatTuple root file or txt file with list of root files] -o [operations to perform] -d [output path]'
       sys.exit()
     elif opt == '-c':
       useCondor = 1
@@ -32,6 +36,7 @@ def getOptions(argv):
   return [useCondor,tTree , inFile, operationList, outputDir]
 
 def editAnalysisDriver(List):
+  txtFileList = ""
   analysisDriverFileTmp = open(os.getcwd() + '/src/analysisDriver.cc.tmp')
   analysisDriverFileUse = open(os.getcwd() + '/src/analysisDriver.cc','w')
   for line in analysisDriverFileTmp:
@@ -44,41 +49,64 @@ def editAnalysisDriver(List):
       line = '  TChain * T = new TChain("' + List[1] + '");' + '\n'
     if 'Add root files here' in line:
       if '.root' in List[2]:
-        line = '  T->Add("' + List[2] + '");' + '\n'
+        if List[0] == 1:
+            inFileTitle = List[2].rsplit('/',1)[1]
+        else:
+            inFileTitle = List[2]
+        line = '  T->Add("' + inFileTitle + '");' + '\n'
       elif '.txt' in List[2]:
         addFileString = ""
         fileTxt = open(os.getcwd() + '/' + List[2])
         for line in fileTxt:
-          addFileString += '  T->Add' + line + '\n'
+          txtFileList += str(line) + ","
+          if List[0] == 1:
+            inFileTitle = line.rsplit('/',1)[1]
+          else:
+            inFileTitle = line
+          inFileTitle = inFileTitle.replace("\n","")
+          addFileString += '  T->Add("' + inFileTitle + '");' + '\n'
         fileTxt.close()
         line = addFileString
     analysisDriverFileUse.write(line)
   analysisDriverFileUse.close()
   analysisDriverFileTmp.close()
+  txtFileList = txtFileList.replace("\n","")
+  return txtFileList
 
-def editCondorConfig(List):
+def editCondorConfig(List,execTitle):
   condorConfigFileTmp = open(os.getcwd() + '/run_condor.txt.tmp')
   condorConfigFileUse = open(os.getcwd() + '/run_condor.txt', 'w')
   for line in condorConfigFileTmp:
     if 'Place Directory' in line:
       line = 'Initialdir = ' + List[4] + '\n'
+    if 'Place Input' in line:
+      if '.root' in List[2]:
+        line = 'transfer_input_files = ' + List[2] + ',/uscms/home/gfunk/public/pDistPlots.root,/uscms/home/gfunk/public/dRvP_singleSample2.root' + '\n'
+      else:
+        line = 'transfer_input_files = ' + txtFileListFinal + '/uscms/home/gfunk/public/pDistPlots.root,/uscms/home/gfunk/public/dRvP_singleSample2.root' + '\n'
+    if 'Place Executable' in line:
+      line = 'Executable = ' + execTitle + '\n'
     condorConfigFileUse.write(line)
   condorConfigFileUse.close()
   condorConfigFileTmp.close()
 
 allParams = getOptions(sys.argv[1:])
-print allParams
-editAnalysisDriver(allParams)
-editCondorConfig(allParams)
+print allParams[2]
+if '.root' in allParams[2]:
+  runTitle = 'fileRun'
+else:
+  runTitle = 'RUN_ANALYSIS_' + allParams[2].split('.')[0]
+txtFileListFinal = editAnalysisDriver(allParams)
+editCondorConfig(allParams,runTitle)
 
-#if not os.path.isdir(allParams[4]) == "":
-#  subprocess.call('mkdir ' + allParams[4], shell = True)
 subprocess.call('make clean', shell = True)
 subprocess.call('make dict', shell = True)
 subprocess.call('make', shell = True)
+time.sleep(1)
 if allParams[0] == 1:
+  subprocess.call('mv run_analysis ' + runTitle,shell = True)
   if os.path.isdir(allParams[4]) == 0:
-    subprocess.call('mkdir ' + allParams[4], shell = True)
+    subprocess.call('mkdir -p ' + allParams[4], shell = True)
   subprocess.call('condor_submit run_condor.txt', shell = True)
 else:
   subprocess.call('./run_analysis', shell = True)
